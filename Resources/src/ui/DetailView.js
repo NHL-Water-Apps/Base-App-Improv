e9ac76cf87a1c05e23a6e99e4fb7906f0f308214
Mathijs Jonker <mathijsjonker@msn.com>
2012-06-19 10:34:13 +0200

    Left voor jasper

--- Resources/src/ui/DetailView.js
+++ Resources/src/ui/DetailView.js
@@ -51,7 +51,7 @@
 		// Type van de data
 		TypeData : Titanium.UI.createLabel({
 			text : 			"",
-			textAlign: 		'center',
+			textAlign: 		'Left',
 			left: 			'3 %',
 			width : 		'80%',
 			heigth : 		18,
