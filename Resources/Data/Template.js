d695909dca025227097aea6e5bce233811055610
Marten Schilstra <martenschilstra@gmail.com>
2012-06-21 11:16:56 +0200

    wat help files toegevoegd

---
+++ Resources/Data/Template.js
@@ -0,0 +1,11 @@
+/*
+  Dit is een template voor JS dat voor de water apps
+*/
+module.exports = [
+  /*
+    Voeg hier objecten met data toe, voorbeeld:
+    {"Foo": "xyz", "Bar": 2},
+    {"Foo": "abc", "Bar": 4},
+    etc ..
+  */
+];
